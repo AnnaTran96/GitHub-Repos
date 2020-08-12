7acb133861867e9426919207cc49beca77e68a60
Anna Tran <annatran@annas-air.mynet>
2020-08-12 17:04:37 +0100

    add repo and profile data

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -11,11 +11,11 @@ class Repo extends React.Component {
     renderResults = (results) => {
         return results.map((item, index) => (
             <div key={ index } className="results">
-                <a href={item.html_url} target="_blank">{item.name}</a>
+                <a href={item.html_url} target="_blank" rel="noopener noreferrer">{item.name}</a>
                 <ul>
-                    <li>Open Issues: {item.open_issues_count}</li>
-                    <li>Stargazers: {item.stargazers_count}</li>
-                    <li>Forks: {item.forks}</li>
+                    {/* <span className="circle"></span> */}
+                    <li>{item.language}</li>
+                    <i className="fa fa-code-fork" aria-hidden="true"><li>{item.forks}</li></i>
                 </ul>
             </div>
         ))
@@ -24,8 +24,12 @@ class Repo extends React.Component {
     renderAvatar = results => {
         return(
             <div className="avatar">
-                <img src={results[0].owner.avatar_url} className="avataricon"></img>
-                <h1>{results[0].owner.login}</h1>
+                <img src={results[0].owner.avatar_url} className="avataricon" alt="Avatar"></img>
+                <h1 className="gitName">{results[0].owner.login}</h1>
+                <a href={results[0].owner.html_url} className="profile" target="_blank" rel="noopener noreferrer">View Profile</a>
+                <br></br>
+                <i class="fa fa-star-o" aria-hidden="true"><span>{results[0].stargazers_count}</span></i>
+                <p className="repos">Repositories ({this.props.results.length})</p>
             </div>
         )
     }
@@ -35,7 +39,10 @@ class Repo extends React.Component {
        
         return(
             <>
-                <h1 className="title">GitHub Repositories</h1>
+                <div className="title">
+                    <i className="fa fa-github" aria-hidden="true"></i>
+                    <h1 >GitHub Repositories</h1>
+                </div>
                 <SearchBar getResult={ this.getResult } />
                 { this.props.results ? this.renderAvatar(this.props.results) : "" }
                 { this.props.results ? this.renderResults(this.props.results) : <p className="intro">Search a user's repositories above</p> }
